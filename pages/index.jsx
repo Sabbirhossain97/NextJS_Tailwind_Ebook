0393c2f52ea07167a3894eba0da6c9c7443aa8c8
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2023-05-16 00:06:25 +0600

    added next config file

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { supabase } from "../api";
 import Link from "next/link";
 import Navbar from "./Navbar";
